289dda4a2c79844d9da67a8c40dc2074971ac460
syouki <syouki2323@gmail.com>
2019-07-18 10:52:45 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,22 @@
 $(document).on('turbolinks:load', function(){
   function buildHTML(message){
-    var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}>
+    // var content = message.content ? `${ message.content }` : "";
+    var image = (message.image)? `<image class="lower-message__image" src="${message.image}">`:"";
+    console.log(image)
+    var html = `<div class="message" data-message-id="${message.id}>
                   <div class="upper-message">
                     <div class="upper-message__user-name">
-                        ${message.user_name}
+                      ${message.user_name}
                     </div>
                     <div class="upper-message__date">
-                        ${message.date}
+                      ${message.date}
                     </div>
                   </div>
                   <div class="lower-message">
                     <p class="lower-message__content">
                       ${message.content}
                     </p>
-                      ${img}
+                    ${image}
                   </div>
                 </div>`
   return html;
@@ -35,10 +36,9 @@ $(document).on('turbolinks:load', function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.chat_box__messages').append(html);
+      $('.chat_box__messages__message').append(html);
       $('#new_message')[0].reset();
-      $('.chat_box__messages').animate({scrollTop: $('.chat_box__messages')[0].scrollHeight}, 'fast');
-      console.log(html)
+      $('.chat_box__messages__message').animate({scrollTop: $('.chat_box__messages__message')[0].scrollHeight}, 'fast');
     })
     .fail(function(data){
       alert('エラーが発生したためメッセージ送信できませんでした。');
@@ -47,4 +47,41 @@ $(document).on('turbolinks:load', function(){
       $('.chat_box__form__new_message__submit').prop('disabled', false);
     })
   })
+
+
+  var reloadMessages = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      $('.chat_box__messages').animate({scrollTop: $('.chat_box__messages')[0].scrollHeight}, 'fast');
+
+      var last_message_id = $('.message:last').data('message-id')
+      console.log(last_message_id)
+
+      var group_id = $('.chat_box__header__groupname__left_box').data('id');
+      // console.log(group_id);
+      // console.log(last_message_id);
+      var href = "/groups/" + group_id + "/api/messages";
+      // console.log(href)
+      $.ajax({
+        url: href,
+        type: "GET",
+        data: {last_id: last_message_id},
+        dataType: "json",
+      })
+      .done(function(data) {
+        var insertHTML = "" ;
+        data.forEach(function(message) {
+          var insertHTML = buildHTML(message)
+          $('.chat_box__messages').append(insertHTML)
+          $('.chat_box__messages').animate({scrollTop: $('.chat_box__messages')[0].scrollHeight}, 'fast');
+          // alert("成功")
+        })
+      })
+      .fail(function(data) {
+        console.log('自動更新に失敗しました');
+      });
+    } else {
+        clearInterval(reloadMessages);
+      }
+  } , 5000 );
 });
+// });
