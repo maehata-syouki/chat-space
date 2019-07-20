6482ff1de1df5cefbdc85e8322a6564cdc7f3a81
syouki <syouki2323@gmail.com>
2019-07-20 18:31:56 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,9 +63,9 @@
 server '3.113.190.55',
   user: 'ec2-user',
   roles: %w{app db web}
-  # ssh_options: {
-  #   port:  22,
-  #   user: 'ec2-user',
-  #   keys: %w(~/.ssh/csgo.pem),
-  #   forward_agent: true,
-  # }
+  ssh_options: {
+    port:  22,
+    user: 'ec2-user',
+    keys: %w(~/.ssh/csgo.pem),
+    forward_agent: true,
+  }
