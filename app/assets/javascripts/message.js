7703e4db50d91a42990e59ca64f6d8d295de231c
syouki <syouki2323@gmail.com>
2019-07-20 13:23:22 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(document).on('turbolinks:load', function(){
   function buildHTML(message){
+    var image = (message.image.url)? `<image class="lower-message__image" src="${message.image.url}">`:"";
     var html = `<div class="message" data-message-id="${message.id}>
                   <div class="upper-message">
                     <div class="upper-message__user-name">
