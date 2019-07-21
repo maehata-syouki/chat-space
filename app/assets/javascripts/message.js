7711dcfeac681ce3bbf41d4bd0d9a39627c50d9d
syouki <syouki2323@gmail.com>
2019-07-21 17:18:21 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,7 +20,6 @@ $(document).on('turbolinks:load', function(){
     return html;
   }
 
-  // メッセージ送信の非同期化部分
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%" ,current_user)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
   def edit
   end
 
@@ -19,7 +11,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%" ,current_user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
