a8428a0f172488d76ce3800d88ed36246d282d40
syouki <syouki2323@gmail.com>
2019-07-21 14:50:03 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'upload-chat-space.test'
-  config.asset_host = 'https://northeast-1.amazonaws.com/bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-chat-space.test'
 end
